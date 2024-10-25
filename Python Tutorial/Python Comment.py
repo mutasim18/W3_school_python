33bc71e8729a23491f607ba52e2ada9648fcef5d
Mutasim Mahmud <mahmudmutasim@gmail.com>
2024-10-25 19:26:21 -0400

    1

---
+++ Python Tutorial/Python Comment.py
@@ -0,0 +1,15 @@
+# This is a comment
+print("We use # for commenting in python") # This is also a comment
+
+# Multi Line comment
+#This is a comment
+#written in
+#more than just one line
+
+"""
+This is a comment
+in 
+More than 
+One line
+"""
+print("YO")
---
+++ Python Tutorial/Python Syntax.py
@@ -0,0 +1,12 @@
+# Python uses indentation to indicate a block of code.
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error.
+if 5>2:
+    print("Five greater than two")
+
+# Python Variables
+x = 5
+y = "Hello World"
+
+# Comments
+# This is a comment
+
