9d1b4f44e62f6d0b75e1999d2984da8798d5afc9
Mutasim Mahmud <mahmudmutasim@gmail.com>
2024-10-26 18:29:00 -0400

    1

---
+++ Python Tutorial/escape characters.py
@@ -0,0 +1,16 @@
+# escape characters:
+"""
+\'	Single Quote	
+\"  Double Quote
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+"""
+
+# example of \"
+print("It has always beed \"The D\" family")
