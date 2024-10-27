f236ca1b68aa615f2a9beb660d7592835d7e12fb
Mutasim Mahmud <mahmudmutasim@gmail.com>
2024-10-26 20:49:59 -0400

    1

---
+++ Python Tutorial/Add list items.py
@@ -0,0 +1,20 @@
+# append
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+# insert
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+# extend
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
