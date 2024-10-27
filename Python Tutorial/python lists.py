4fb39bfcc76301116b1d281da0b004020fa62618
Mutasim Mahmud <mahmudmutasim@gmail.com>
2024-10-26 20:05:35 -0400

    1

---
+++ Python Tutorial/python lists.py
@@ -0,0 +1,24 @@
+"""
+There are four collection data types in the Python programming language:
+
+-List is a collection which is ordered and changeable. Allows duplicate members.
+-Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+-Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+-Dictionary is a collection which is ordered** and changeable. No duplicate members.
+"""
+
+"""
+# List
+List items are indexed, the first item has index [0], the second item has index [1]
+# List are ordered
+# we can change values on a list
+# Since lists are indexed, lists can have items with the same value
+# List items can be of any data type
+"""
+mylist = ["abc", 34, True, 40, "male"]
+print(type(mylist))
+
+# list()
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+
